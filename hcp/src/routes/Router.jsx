d2c00e8ac2e095aac4b2a8df27893b31e1fc8605
pro660 <gw904202@gmail.com>
2026-03-01 22:24:50 +0900

    라우터 수정

--- hcp/src/routes/Router.jsx
+++ hcp/src/routes/Router.jsx
@@ -1,17 +1,12 @@
-// src/routes/Router.jsx (전체 교체본 - 동아리 상세 + 소개글 입력(관리) 페이지 반영)
+// src/routes/Router.jsx (전체 교체본 - signup 라우트/오버레이 모두 제거)
 import React from "react";
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 
 import MobileAppLayout from "../components/layout/MobileAppLayout";
-import SlideOverlay from "../components/transition/SlideOverlay";
 
 import SplashPage from "../pages/splash/SplashPage";
 import LoginPage from "../pages/auth/LoginPage";
 import AdminLoginPage from "../pages/auth/AdminLoginPage";
-
-import SignupVerifyPage from "../pages/auth/SignupVerifyPage";
-import SignupStep2Page from "../pages/auth/SignupStep2Page";
 
 import AppShell from "../components/AppShell";
 import AdminRoute from "./AdminRoute";
@@ -27,14 +22,13 @@ import ApplicantsPage from "../pages/mypage/ApplicantsPage";
 // ✅ 소개글 입력(동아리 글 생성/수정) 페이지
 import IntroEdit from "../pages/mypage/IntroEdit";
 
-// ✅ 지원서 양식/열람 (파일명 단순화)
+// ✅ 지원서 양식/열람
 import ApplyWrite from "../pages/apply/ApplyWrite";
 import ApplyRead from "../pages/apply/ApplyRead";
 
 export default function Router() {
   const location = useLocation();
   const baseLocation = location.state?.backgroundLocation;
-  const prevOverlayLocation = location.state?.prevOverlayLocation;
 
   return (
     <MobileAppLayout>
@@ -82,7 +76,7 @@ export default function Router() {
             }
           />
 
-          {/* ✅ 소개글 입력(관리자만) -> 동아리 글 생성/수정 페이지 */}
+          {/* ✅ 소개글 입력(관리자만) */}
           <Route
             path="/mypage/intro"
             element={
@@ -102,50 +96,8 @@ export default function Router() {
           />
         </Route>
 
-        <Route path="/signup" element={<SignupVerifyPage />} />
-        <Route path="/signup/step2" element={<SignupStep2Page />} />
-
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-
-      {prevOverlayLocation ? (
-        <Routes
-          location={prevOverlayLocation}
-          key={`prev-${prevOverlayLocation.key}`}
-        >
-          <Route
-            path="/signup"
-            element={
-              <SlideOverlay animated={false}>
-                <SignupVerifyPage />
-              </SlideOverlay>
-            }
-          />
-        </Routes>
-      ) : null}
-
-      <AnimatePresence initial={false}>
-        {baseLocation ? (
-          <Routes location={location} key={`cur-${location.key}`}>
-            <Route
-              path="/signup"
-              element={
-                <SlideOverlay>
-                  <SignupVerifyPage />
-                </SlideOverlay>
-              }
-            />
-            <Route
-              path="/signup/step2"
-              element={
-                <SlideOverlay>
-                  <SignupStep2Page />
-                </SlideOverlay>
-              }
-            />
-          </Routes>
-        ) : null}
-      </AnimatePresence>
     </MobileAppLayout>
   );
 }
